56265657cb82f56644c9d80699148146dfd98123
jsladea <jaaronslade@gmail.com>
2024-11-25 16:32:09 -0700

    Added test for update widget

--- test.py
+++ test.py
@@ -106,12 +106,12 @@ class TestClass(unittest.TestCase):
         assert 'Item' not in result.keys()
 
     @mock_aws
-    def test_process_widget_correctlydeletesdynamodata(self):
+    def test_process_widget_correctlyupdatessdynamodata(self):
         conn = boto3.resource('s3', region_name='us-east-1')
         conn.create_bucket(Bucket=main.bucket_name)
         bucket = conn.Bucket(main.bucket_name)
         test_widget_filename = "test-widget.json"
-        test_delete_widget_filename = "test-delete-widget.json"
+        test_update_widget_filename = "test-update-widget.json"
         bucket.upload_file(test_widget_filename, str(1))
         widget_data = dict()
         with open(test_widget_filename, 'r') as file:
@@ -138,11 +138,19 @@ class TestClass(unittest.TestCase):
         )
         table = dynamodb.Table(main.table_name)
         main.process_widget(widget_data)
-        with open(test_delete_widget_filename, 'r') as file:
+        with open(test_update_widget_filename, 'r') as file:
             widget_data = json.load(file)
         main.process_widget(widget_data)
-        result = table.get_item(Key={'id': widget_data['widgetId']})
-        assert 'Item' not in result.keys()
+        widget_record = table.get_item(Key={'id': widget_data['widgetId']})['Item']
+        assert widget_record['id'] == widget_data['widgetId']
+        assert widget_record['owner'] == widget_data['owner']
+        assert widget_record['label'] == widget_data['label']
+        assert widget_record['description'] == widget_data['description']
+        assert widget_record['size-unit'] == widget_data['otherAttributes'][0]['value']
+        assert widget_record['height'] == widget_data['otherAttributes'][1]['value']
+        assert widget_record['length'] == widget_data['otherAttributes'][2]['value']
+        assert widget_record['price'] == widget_data['otherAttributes'][3]['value']
+        assert widget_record['vendor'] == widget_data['otherAttributes'][4]['value']
 
 
 
